635d977822c88e0cff32abbc2aa435ad075280dc
Dipak Kumawat <dipaksk11@gmail.com>
2024-11-12 21:11:04 +0530

    problem solving of version conflict

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,23 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dns from 'dns'
 
-dns.setDefaultResultOrder('verbatim')
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [react()],
-    server: {
-      port: 3000
-    },
-    define: {
-      'process.env': env,
-    },
-    build: {
-      outDir: 'dist',
-      emptyOutDir: true
-    }
-  };
-});
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
