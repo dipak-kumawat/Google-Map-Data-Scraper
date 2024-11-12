3057161359aec578c9d543a6f3a4840187b8ca95
Dipak Kumawat <dipaksk11@gmail.com>
2024-11-12 20:31:07 +0530

    changing the vite file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import dns from 'dns'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+dns.setDefaultResultOrder('verbatim')
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000
+    },
+    define: {
+      'process.env': env,
+    },
+    build: {
+      outDir: './dist',
+      emptyOutDir: true
+    }
+  };
+});
